28ca394d7963f8febb491868a64c78bd81cc87bc
Mohamed Eltuhamy <tuhamy@gmail.com>
2013-03-03 17:03:02 +0000

    better looking button

--- chrome-extension/quranslides.js
+++ chrome-extension/quranslides.js
@@ -1,5 +1,5 @@
 console.log("Quran slides loaded");
 
 $(document).ready(function(){
-  $('#title').after('<a style="  color: white; text-decoration: none; font-size: 16px; float: left; background: green; border-radius: 4px; padding: 4px; padding: 4px; margin-top: 4px; margin-right: 10px;" href="http://www.doc.ic.ac.uk/~me1810/quran-slides/?1:1-7">View as presentation</a>');
+  $('#title').after('<a style="  color: white; text-decoration: none; font-size: 15px; float: left; background: #79872F; border-radius: 4px; padding: 4px; padding: 4px; margin-top: 4px; margin-right: 10px;" href="http://www.doc.ic.ac.uk/~me1810/quran-slides/?1:1-7">View as presentation</a>');
 });
