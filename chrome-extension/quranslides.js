ad25b5f7206651b0637335f6dbe871f8921a79a0
Mohamed Eltuhamy <tuhamy@gmail.com>
2013-03-03 17:22:04 +0000

    Extension now parses url correctly

--- chrome-extension/quranslides.js
+++ chrome-extension/quranslides.js
@@ -1,5 +1,13 @@
 console.log("Quran slides loaded");
 
-$(document).ready(function(){
-  $('#title').after('<a style="  color: white; text-decoration: none; font-size: 15px; float: left; background: #79872F; border-radius: 4px; padding: 4px; padding: 4px; margin-top: 4px; margin-right: 10px;" href="http://www.doc.ic.ac.uk/~me1810/quran-slides/?1:1-7">View as presentation</a>');
-});
+var url = window.location.pathname;
+url = url.substring(1).split('/');
+var surahPart = parseInt(url[0]);
+var ayaPart = parseInt(url[1]);
+url = surahPart;
+if(!isNaN(surahPart)){
+  if(!isNaN(ayaPart)){
+    url += ':' + ayaPart;
+  }
+  $('#title').after('<a style="  color: white; text-decoration: none; font-size: 15px; float: left; background: #79872F; border-radius: 4px; padding: 4px; padding: 4px; margin-top: 4px; margin-right: 10px;" href="http://www.doc.ic.ac.uk/~me1810/quran-slides/?'+url+'">View as presentation</a>');
+}
